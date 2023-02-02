a0a7a64de3609699fb475888459b3cbe414264f2
Ocksana Bondarenko <bondarrren777@gmail.com>
2023-02-02 16:29:19 +0200

    3

---
+++ app/utils/localStorageUtil.js
@@ -0,0 +1,29 @@
+class LocalStorageUtil {
+  constructor() {
+    this.keyName = "products";
+  }
+  getProducts() {
+    const productsLocalStorage = localStorage.getItem(this.keyName);
+    if (productsLocalStorage !== null) {
+      return JSON.parse(productsLocalStorage);
+    }
+    return [];
+  }
+  putProducts(id) {
+    let products = this.getProducts();
+    const index = products.indexOf(id);
+
+    if (index === -1) {
+      products.push(id);
+    } else {
+      products.splice(index, 1);
+    }
+
+    localStorage.setItem(this.keyName, JSON.stringify(products));
+  }
+}
+
+const localStorageUtil = new LocalStorageUtil();
+
+localStorageUtil.putProducts("el1");
+localStorageUtil.putProducts("el2");
